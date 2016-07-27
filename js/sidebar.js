89f10a430118d553063befc0aa9d1f92c65accf5
Jacob <jacob.varner14@gmail.com>
2016-07-27 09:39:35 -0500

    Added basic styling to the sidebar and implemented the slide in and out animation.

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,12 +3,12 @@ function toggleMenu() {
 
   if (menuStatus === "closed") {
     console.log("Open!");
-    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("sidebar").style.right = "0";
     document.getElementById("menu-icon").src = "/assets/images/menu_close.png";
     document.getElementById("menu-icon").className = "open";
   } else {
     console.log("Close!");
-    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("sidebar").style.right = "-300px";
     document.getElementById("menu-icon").src = "/assets/images/menu_open.png";
     document.getElementById("menu-icon").className = "closed";
   }
