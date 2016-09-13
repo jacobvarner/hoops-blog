cec2da3f33c27264b3d061d3d798cc744fdc6cb6
Jacob <jacob.varner14@gmail.com>
2016-09-13 16:40:57 -0500

    Added an advertisement to the index of posts and fixed the JavaScript for the sidebar using some delays to make the sidebar 'disappear' once it moves off screen.

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,12 +3,14 @@ function toggleMenu() {
 
   if (menuStatus === "closed") {
     console.log("Open!");
-    document.getElementById("sidebar").style.right = "0px";
+    document.getElementById("sidebar").style.display = "block";
+    setTimeout(function(){document.getElementById("sidebar").style.right = "0px"}, 50);
     document.getElementById("menu-icon").src = "/assets/images/menu_close.png";
     document.getElementById("menu-icon").className = "open";
   } else {
     console.log("Close!");
     document.getElementById("sidebar").style.right = "-300px";
+    setTimeout(function(){document.getElementById("sidebar").style.display = "none"}, 300);
     document.getElementById("menu-icon").src = "/assets/images/menu_open.png";
     document.getElementById("menu-icon").className = "closed";
   }
