0e6d66a0efa1994f1e15bfb19a0a4d13826df1b5
Jacob <jacob.varner14@gmail.com>
2016-09-23 19:21:26 -0500

    Fixed the js links for pages other than the homepage.

--- js/sidebar.js
+++ js/sidebar.js
@@ -5,13 +5,13 @@ function toggleMenu() {
     console.log("Open!");
     document.getElementById("sidebar").style.display = "block";
     setTimeout(function(){document.getElementById("sidebar").style.right = "0px"}, 50);
-    document.getElementById("menu-icon").src = "~/assets/images/menu_close.png";
+    document.getElementById("menu-icon").src = "/assets/images/menu_close.png";
     document.getElementById("menu-icon").className = "open";
   } else {
     console.log("Close!");
     document.getElementById("sidebar").style.right = "-300px";
     setTimeout(function(){document.getElementById("sidebar").style.display = "none"}, 300);
-    document.getElementById("menu-icon").src = "~/assets/images/menu_open.png";
+    document.getElementById("menu-icon").src = "/assets/images/menu_open.png";
     document.getElementById("menu-icon").className = "closed";
   }
 }
