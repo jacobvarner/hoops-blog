153305c17cab2e3aefa5279918c7b9bbe36747d0
Jacob <jacob.varner14@gmail.com>
2016-09-23 20:02:32 -0500

    Ugh.

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,13 +1,11 @@
 function toggleMenu() {
-  var menuStatus = document.getElementById("menu-icon").className;
 
-  if (menuStatus === "closed") {
+  if (document.getElementById("menu-icon-closed").style.display == "block") {
     console.log("Open!");
     document.getElementById("sidebar").style.display = "block";
     setTimeout(function(){document.getElementById("sidebar").style.right = "0px"}, 50);
     document.getElementById("menu-icon-closed").style.display = "none";
     document.getElementById("menu-icon-open").style.display = "block";
-    document.getElementById("menu-icon").className = "open";
   } else {
     console.log("Close!");
     document.getElementById("sidebar").style.right = "-300px";
